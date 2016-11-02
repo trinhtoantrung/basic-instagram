f3c6265ff308e8758b39f99bef083d331f6ba2f0
tttrinh <trinhtoantrung@gmail.com>
2016-11-02 14:45:46 +0700

    Sort by categore or date - DONE

--- app/assets/javascripts/controllers/MyInstagramController.js
+++ app/assets/javascripts/controllers/MyInstagramController.js
@@ -10,9 +10,37 @@ angular.module('BasicInstagram').controller('MyInstagramController', function($s
 
 	$scope.sorts = [["category", "ASC"], ["category", "DESC"], ["date", "ASC"], ["date", "DESC"]];
 	$scope.sort = $scope.sorts[0];
+	$scope.sortBy = "+category";
 
 	$scope.chooseCategory = function(pCategory) {
 		$scope.category = pCategory;
+	}
+
+	$scope.orderBy = function(sort) {
+		$scope.sort = sort;
+		if (sort) {
+			if (sort[1] === "ASC") {
+				$scope.sortBy = "+";
+			} else {
+				$scope.sortBy = "-";
+			}
+
+			if (sort[0] === "category") {
+				$scope.sortBy += "category";
+			} else {
+				$scope.sortBy += "date";
+			}
+		}
+	}
+
+
+	// in this case, we are able to use the default comparator enough
+	$scope.myOrderComparator = function(image1, image2) {
+		if ($scope.sort[0] === "date") {
+			return (image1.value.date < image2.value.date) ? -1 : 1;
+		} else if ($scope.sort[0] === "category") {
+			return (image1.value.category < image2.value.category) ? -1 : 1;
+		}
 	}
 
 	var data = new Image().getService().query(function() {
